502cb6df7f0e7233abfc0fafb6c89c1db56fcc00
Roseanne Gerin <rg2875@columbia.edu>
2014-04-05 16:55:24 -0400

    Second Commit

--- PhotoScript.js
+++ PhotoScript.js
@@ -3,19 +3,25 @@
  */
 
 
-/*This is an array of all our images in order)*/
-var images = ["drain_0m.png", "drain_50m.png", "drain_100m.png", "drain_200m.png", "drain_500m.png",
- "drain_1km.png", "drain_2km.png", "drain_3km.png", "drain_5km.png", "drain_ed.png", "drain_nl.png"];
+/*This is an array of the photos.*/
+var images = ["Chrysanthemum.jpg", "Desert.jpg", "Hydrangeas.jpg", "Jellyfish.jpg", "Koala.jpg",
+ "Lighthouse.jpg", "Penguins.jpg", "Tulips.jpg"];
+ 
 
-/* Index of the current image in the array */
+/* Index of the current photos in the array */
 var index=0;
 
-
+/*I am setting button events here with "setNav".*/
 function setNav() {
 	console.log("setNav");
-	
+/*".button" will select anyting with a class of button in it.*/	
 	$(".button").on("click", function() {
-		
+/*This is where somthing happens when ".button" elements are clicked.*/		
+		console.log("click");
+
+/*Next, I'll add if/else statments to tell the computer to go tot eh next photo
+ * for var ".hasClass" then "next".
+ */		
 		var isNext = $(this).hasClass("next");
 		if (isNext == true && index != (images.length-1)) {
 			index = index + 1;
@@ -38,7 +44,7 @@ function setNav() {
 	
 }
 
-/* Change the image here */
+/* This function will change the photos.*/
 function updateImage() {
 	console.log(images[index]);
 	$(".image-holder").html(
@@ -46,7 +52,7 @@ function updateImage() {
 	);
 }
 
-/* Load the first image into our image holder */
+/* This comment loads the first image into the image holder */
 $(document).ready(function() {
 	$(".image-holder").html(
 		"<img src='images/"+images[index]+"'/>"
